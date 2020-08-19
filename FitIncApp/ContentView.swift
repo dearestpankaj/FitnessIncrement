444f1d7829722afedaf26f884a227e7755417780
dearestpankaj <dearestpankaj@gmail.com>
2020-08-20 00:07:47 +0530

    Initial Commit

---
+++ FitIncApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitIncApp
+//
+//  Created by Pankaj Sachdeva on 20/08/20.
+//  Copyright © 2020 Pankaj Sachdeva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
